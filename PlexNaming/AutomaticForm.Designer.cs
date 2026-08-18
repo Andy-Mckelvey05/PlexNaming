bc7bc80cc554ff79920769a40f6af0e1ce785976
Andy-Mckelvey05 <amckelveypersonal@gmail.com>
2026-08-18 19:48:46 +0100

    Added TVDB Scraping

--- PlexNaming/AutomaticForm.Designer.cs
+++ PlexNaming/AutomaticForm.Designer.cs
@@ -30,13 +30,17 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutomaticForm));
             buttonManualForm = new Button();
+            textBoxTVDBLink = new TextBox();
+            textBox1 = new TextBox();
+            buttonCheckResults = new Button();
+            listBoxExamples = new ListBox();
             SuspendLayout();
             // 
             // buttonManualForm
             // 
             buttonManualForm.BackColor = Color.FromArgb(255, 190, 40);
             buttonManualForm.Font = new Font("Courier New", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            buttonManualForm.Location = new Point(12, 23);
+            buttonManualForm.Location = new Point(443, 393);
             buttonManualForm.Name = "buttonManualForm";
             buttonManualForm.Size = new Size(257, 120);
             buttonManualForm.TabIndex = 7;
@@ -44,12 +48,59 @@
             buttonManualForm.UseVisualStyleBackColor = false;
             buttonManualForm.Click += buttonManualForm_Click;
             // 
+            // textBoxTVDBLink
+            // 
+            textBoxTVDBLink.BackColor = Color.FromArgb(250, 210, 37);
+            textBoxTVDBLink.Font = new Font("Courier New", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBoxTVDBLink.Location = new Point(12, 12);
+            textBoxTVDBLink.Name = "textBoxTVDBLink";
+            textBoxTVDBLink.PlaceholderText = "TVDB Season Link?";
+            textBoxTVDBLink.Size = new Size(711, 38);
+            textBoxTVDBLink.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(250, 210, 37);
+            textBox1.Font = new Font("Courier New", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(12, 56);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Show Name?";
+            textBox1.Size = new Size(711, 38);
+            textBox1.TabIndex = 9;
+            // 
+            // buttonCheckResults
+            // 
+            buttonCheckResults.BackColor = Color.FromArgb(255, 190, 40);
+            buttonCheckResults.Font = new Font("Courier New", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            buttonCheckResults.Location = new Point(28, 393);
+            buttonCheckResults.Name = "buttonCheckResults";
+            buttonCheckResults.Size = new Size(266, 120);
+            buttonCheckResults.TabIndex = 10;
+            buttonCheckResults.Text = "Check Results";
+            buttonCheckResults.UseVisualStyleBackColor = false;
+            buttonCheckResults.Click += buttonCheckResults_Click;
+            // 
+            // listBoxExamples
+            // 
+            listBoxExamples.BackColor = Color.FromArgb(250, 210, 37);
+            listBoxExamples.Font = new Font("Courier New", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            listBoxExamples.FormattingEnabled = true;
+            listBoxExamples.HorizontalScrollbar = true;
+            listBoxExamples.Location = new Point(12, 100);
+            listBoxExamples.Name = "listBoxExamples";
+            listBoxExamples.Size = new Size(711, 244);
+            listBoxExamples.TabIndex = 11;
+            // 
             // AutomaticForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(27, 27, 27);
-            ClientSize = new Size(289, 170);
+            ClientSize = new Size(735, 559);
+            Controls.Add(listBoxExamples);
+            Controls.Add(buttonCheckResults);
+            Controls.Add(textBox1);
+            Controls.Add(textBoxTVDBLink);
             Controls.Add(buttonManualForm);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
@@ -58,9 +109,14 @@
             Name = "AutomaticForm";
             Text = "PlexTvNaming";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
         private Button buttonManualForm;
+        private TextBox textBoxTVDBLink;
+        private TextBox textBox1;
+        private Button buttonCheckResults;
+        private ListBox listBoxExamples;
     }
 }
--- PlexNaming/AutomaticForm.cs
+++ PlexNaming/AutomaticForm.cs
@@ -14,5 +14,50 @@ namespace PlexNaming
             ManualForm manualForm = new();
             manualForm.ShowDialog();
         }
+
+        private void buttonCheckResults_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBoxExamples.Items.Clear();
+
+                string tvdbUrl = textBoxTVDBLink.Text.Trim();
+
+                if (string.IsNullOrWhiteSpace(tvdbUrl))
+                {
+                    MessageBox.Show("Please enter a TVDB season URL.");
+                    return;
+                }
+
+                buttonCheckResults.Enabled = false;
+
+                TVDBScraper scraper = new TVDBScraper();
+                TVDBSeason season = scraper.GetSeason(tvdbUrl);
+
+                listBoxExamples.Items.Add($"Show: {season.ShowName}");
+                listBoxExamples.Items.Add($"Season: {season.SeasonNumber:D2}");
+                listBoxExamples.Items.Add($"Episodes: {season.Episodes.Count}");
+                listBoxExamples.Items.Add("----------------------------------------");
+
+                foreach (TVDBEpisode episode in season.Episodes)
+                {
+                    string formattedName =
+                        $"{season.ShowName} - " +
+                        $"season {season.SeasonNumber:D2} - " +
+                        $"s{season.SeasonNumber:D2}e{episode.Number:D2} - " +
+                        $"{episode.Name}";
+
+                    listBoxExamples.Items.Add(formattedName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"TVDB Scraper Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                buttonCheckResults.Enabled = true;
+            }
+        }
     }
 }
